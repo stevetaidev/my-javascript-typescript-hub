6fbc83d9b3e479d2cbffecdf5fa81835a55f7908
stevetaidev <stevetaidev@gmail.com>
2023-12-24 23:52:39 +0700

    add NotANumber.js

--- Javascript/3. Numbers In-Depth/2.NotANumber.js
+++ Javascript/3. Numbers In-Depth/2.NotANumber.js
@@ -1,2 +1,5 @@
 let result = "Hello" / 2; // Kết quả là NaN vì không thể chia một chuỗi cho một số
 console.log(result); 
+
+let num = parseInt("abc"); // num sẽ có giá trị NaN vì chuỗi "abc" không thể chuyển đổi thành số.
+console.log(num); 
