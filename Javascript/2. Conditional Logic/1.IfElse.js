9ffdc1092b9264dc0f8aaa7c1f74fa08699ec298
stevetaidev <stevetaidev@gmail.com>
2023-12-24 23:26:14 +0700

    if else statement

---
+++ Javascript/2. Conditional Logic/1.IfElse.js
@@ -0,0 +1,9 @@
+let age = 10;
+
+if (age < 18) {
+  console.log("Bạn là một người trẻ em.");
+} else if (age >= 18 && age < 60) {
+  console.log("Bạn là một người trưởng thành.");
+} else {
+  console.log("Bạn là một người già.");
+}
