f99a2a2a9defb180311e8a9eb561279017e3c278
stevetaidev <stevetaidev@gmail.com>
2023-12-25 00:20:31 +0700

    add TypeOf

--- Javascript/4. Strings In-Depth/2.TypeOf.js
+++ Javascript/4. Strings In-Depth/2.TypeOf.js
@@ -5,3 +5,16 @@ if (typeof value === "string") {
 } else {
   console.log("Giá trị không phải là chuỗi.");
 }
+
+
+// Sử dụng hàm instanceof để kiểm tra xem giá trị có phải là một đối tượng chuỗi (String) hoặc một chuỗi (primitive string) không.
+
+let value1 = "Hello, World!";
+
+if (value1 instanceof String) {
+  console.log("Giá trị là một đối tượng chuỗi.");
+} else if (typeof value1 === "string") {
+  console.log("Giá trị là một chuỗi nguyên thủy.");
+} else {
+  console.log("Giá trị không phải là chuỗi.");
+}
