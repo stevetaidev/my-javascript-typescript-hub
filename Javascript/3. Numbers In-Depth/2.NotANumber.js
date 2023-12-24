9fd09670e8bd3fba7265cd7b726892b9e246d1d4
stevetaidev <stevetaidev@gmail.com>
2023-12-24 23:54:05 +0700

    add NotANumber.js

--- Javascript/3. Numbers In-Depth/2.NotANumber.js
+++ Javascript/3. Numbers In-Depth/2.NotANumber.js
@@ -3,3 +3,12 @@ console.log(result);
 
 let num = parseInt("abc"); // num sẽ có giá trị NaN vì chuỗi "abc" không thể chuyển đổi thành số.
 console.log(num); 
+
+// Sử dụng isNaN() là một cách để kiểm tra xem một giá trị có phải là NaN hay không. Ví dụ:
+
+let value = "abc";
+if (isNaN(value)) {
+  console.log("Giá trị không phải là số hợp lệ");
+} else {
+  console.log("Giá trị là số hợp lệ");
+}
