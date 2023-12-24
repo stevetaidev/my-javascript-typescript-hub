ef42186259ea391d52052c765ac7fb01194265e1
stevetaidev <stevetaidev@gmail.com>
2023-12-24 23:43:04 +0700

    add Number.js

---
+++ Javascript/3. Numbers In-Depth/1.Number.js
@@ -0,0 +1,7 @@
+let num1 = 42; // Sử dụng number literal để tạo số nguyên 42
+let num2 = 3.14; // Sử dụng number literal để tạo số thập phân 3.14
+
+
+let str = "123";
+let num = parseInt(str); // Chuyển đổi chuỗi "123" thành số nguyên 123
+let num3 = new Number(42); // Sử dụng hàm tạo Number() để tạo số nguyên 42
