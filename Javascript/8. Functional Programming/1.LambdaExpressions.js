015a62c91cea5f1fcb7b71e83b1f2104a4e2aa9a
stevetaidev <stevetaidev@gmail.com>
2023-12-26 00:36:26 +0700

    Add Arrow Function

---
+++ Javascript/8. Functional Programming/1.LambdaExpressions.js
@@ -0,0 +1,12 @@
+
+
+// Lambda expression (Arrow function) để tính bình phương của một số
+const square = (x) => x * x;
+
+console.log(square(5));
+
+// Lambda expression để double mỗi phần tử trong mảng
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map((x) => x * 2);
+
+console.log(doubledNumbers); 
